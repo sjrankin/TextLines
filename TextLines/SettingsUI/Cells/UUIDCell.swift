d9efb1eb572feef9b86189f3df56fffd6456bde2
Stuart Rankin <sjrankin@gmail.com>
2021-10-15 10:11:52 -0700

    Update UUIDCell.swift
    Minor rename of functions.

--- TextLines/SettingsUI/Cells/UUIDCell.swift
+++ TextLines/SettingsUI/Cells/UUIDCell.swift
@@ -46,14 +46,14 @@ class UUIDCell: UITableViewCell, UITextFieldDelegate
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
                                         target: nil, action: nil)
         let doneButton = UIBarButtonItem(title: "Dismiss", style: .done,
-                                         target: self, action: #selector(DoneButtonTapped))
+                                         target: self, action: #selector(KeyboardDoneButtonTapped))
         
         toolbar.setItems([flexSpace, doneButton], animated: true)
         toolbar.sizeToFit()
         TextField.inputAccessoryView = toolbar
     }
     
-    @objc func DoneButtonTapped()
+    @objc func KeyboardDoneButtonTapped()
     {
         self.contentView.endEditing(true)
     }
