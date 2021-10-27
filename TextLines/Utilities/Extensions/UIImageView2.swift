ece1099b1a20c817f0a89df70a30a51bbcdca4eb
Stuart Rankin <sjrankin@gmail.com>
2021-10-26 21:34:12 -0700

    Create UIImageView2.swift
    Created a very thin wrapper around a UIImageView to provide a .Net-like Tag property.

---
+++ TextLines/Utilities/Extensions/UIImageView2.swift
@@ -0,0 +1,16 @@
+//
+//  UIImageView2.swift
+//  TextLines
+//
+//  Created by Stuart Rankin on 10/24/21.
+//
+
+import Foundation
+import UIKit
+
+/// UIImageView with a .Net-like tag.
+class UIImageView2: UIImageView
+{
+    /// Get or set a tag value of any type. Defaults to `nil`.
+    var Tag: Any? = nil
+}
