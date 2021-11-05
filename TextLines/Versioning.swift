6c0d3f19100be34c999b2a1260b7c1592b08030b
Stuart Rankin <sjrankin@gmail.com>
2021-11-05 15:01:19 -0700

    Update Versioning.swift
    Auto updated.

--- TextLines/Versioning.swift
+++ TextLines/Versioning.swift
@@ -125,19 +125,19 @@ public class Versioning: CustomStringConvertible
     }
     
     /// Build number. Incremented every build (provided the proper script is executed at each build).
-    public static let Build: Int = 1658
+    public static let Build: Int = 1663
     
     /// Build increment. Value to use to increment the build number.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "A458444D-89E9-485B-A846-357CF70EC125"
+    public static let BuildID: String = "5861B1CE-95FB-47DE-94E6-D8D520200709"
     
     /// Build date.
     public static let BuildDate: String = "5 November 2021"
     
     /// Build Time.
-    public static let BuildTime: String = "14:19"
+    public static let BuildTime: String = "14:52"
     
     /// Holds the release build flag.
 #if DEBUG
