9a579419a466aeae177b664f0c0d4780b5fbb0e3
Stuart Rankin <sjrankin@gmail.com>
2021-11-07 19:38:30 -0800

    Create TextFormattingSlice.swift
    Text format settings slice code.

---
+++ TextLines/Main/SliceSettings/TextFormattingSlice.swift
@@ -0,0 +1,58 @@
+//
+//  TextFormattingSlice.swift
+//  TextLines
+//
+//  Created by Stuart Rankin on 11/6/21.
+//
+
+import Foundation
+import UIKit
+
+class TextFormattingSlice: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.view.clipsToBounds = true
+        self.view.layer.cornerRadius = 5.0
+        self.view.layer.borderColor = UIColor.black.cgColor
+        self.view.layer.borderWidth = 1.5
+        
+        //FontColorWell.layer.borderWidth = 0.5
+        //FontColorWell.layer.borderColor = UIColor.black.cgColor
+        //FontColorWell.layer.cornerRadius = 4.0
+        FontColorWell.clipsToBounds = true
+        FontColorWell.selectedColor = Settings.GetColor(.TextColor, .black)
+        FontColorWell.supportsAlpha = true
+        FontColorWell.addTarget(self, action: #selector(TextColorChangedHandler(_:)), for: .valueChanged)
+    }
+    
+    @objc func TextColorChangedHandler(_ sender: Any)
+    {
+        if let Well = sender as? UIColorWell
+        {
+            if let FinalColor = Well.selectedColor
+            {
+                Settings.SetColor(.TextColor, FinalColor)
+            }
+        }
+    }
+    
+    @IBAction func SelectFontTapped(_ sender: Any)
+    {
+    }
+
+    @IBAction func RotateSwitchChangeHandler(_ sender: Any)
+    {
+    }
+    
+    @IBAction func AlignmentControlChanged(_ sender: Any)
+    {
+    }
+    
+    @IBOutlet weak var AlignmentControl: UISegmentedControl!
+    @IBOutlet weak var FontColorWell: UIColorWell!
+    @IBOutlet weak var FontNameLabel: UILabel!
+    @IBOutlet weak var RotateTextSwitch: UISwitch!
+}
