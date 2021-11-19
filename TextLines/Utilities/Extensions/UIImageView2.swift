884c969dce6308494de21d8a6c2354e24bf0e580
Stuart Rankin <sjrankin@gmail.com>
2021-11-19 12:02:11 -0800

    Update UIImageView2.swift
    Added extension for UIImageView to return black backgrounds if the program background is transparent.

--- TextLines/Utilities/Extensions/UIImageView2.swift
+++ TextLines/Utilities/Extensions/UIImageView2.swift
@@ -14,3 +14,24 @@ class UIImageView2: UIImageView
     /// Get or set a tag value of any type. Defaults to `nil`.
     var Tag: Any? = nil
 }
+
+extension UIImageView
+{
+    /// Returns the image with the background color.
+    /// - Returns: Image with the background color or nil on error.
+    func ImageWithBackground() -> UIImage?
+    {
+        guard let Image = self.image else
+        {
+            return nil
+        }
+        let BGColor = Settings.GetColor(.BackgroundColor, UIColor.black)
+        if BGColor.a == 0.0
+        {
+            let BackgroundImage = UIImage(Color: UIColor.black, Size: Image.size)
+            let Blitted = Image.BlitOnBackground(BackgroundImage)
+            return Blitted
+        }
+        return Image
+    }
+}
