556cfb0f1112f75baafd0ca5daf1c80ffde660ac
Stuart Rankin <sjrankin@gmail.com>
2021-11-21 20:24:29 -0800

    Update TextFormattingSlice.swift
    Updated to use UIConstants.

--- TextLines/Main/SliceSettings/TextFormattingSlice.swift
+++ TextLines/Main/SliceSettings/TextFormattingSlice.swift
@@ -15,9 +15,9 @@ class TextFormattingSlice: UIViewController
         super.viewDidLoad()
         
         self.view.clipsToBounds = true
-        self.view.layer.cornerRadius = 5.0
-        self.view.layer.borderColor = UIColor.black.cgColor
-        self.view.layer.borderWidth = 1.5
+        self.view.layer.cornerRadius = UIConstants.CornerRadius
+        self.view.layer.borderColor = UIConstants.DarkBorder
+        self.view.layer.borderWidth = UIConstants.ThickBorder
 
         FontColorWell.clipsToBounds = true
         FontColorWell.selectedColor = Settings.GetColor(.TextColor, .black)
