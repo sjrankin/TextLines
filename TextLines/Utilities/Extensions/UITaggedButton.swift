de7eb99d853305d58a5981efa6a35100e28d1d23
Stuart Rankin <sjrankin@gmail.com>
2021-11-30 19:35:28 -0800

    Create UITaggedButton.swift
    UIButton with a better Tag field.

---
+++ TextLines/Utilities/Extensions/UITaggedButton.swift
@@ -0,0 +1,15 @@
+//
+//  UITaggedButton.swift
+//  TextLines
+//
+//  Created by Stuart Rankin on 11/30/21.
+//
+
+import Foundation
+import UIKit
+
+/// MARK: - UIButton class with a proper `Tag` value.
+class UITaggedButton: UIButton
+{
+    public var Tag: Any? = nil
+}
