d66caa8fa38be359f49119b29e031369ac38b18f
Stuart Rankin <sjrankin@gmail.com>
2021-12-04 13:16:44 -0800

    Update Math.swift
    Added comments.

--- TextLines/ShapeFollowing/Math.swift
+++ TextLines/ShapeFollowing/Math.swift
@@ -9,7 +9,11 @@ import Foundation
 
 class Math
 {
-    //https://www.mathsisfun.com/geometry/ellipse-perimeter.html
+    /// Returns the circumference of an ellipse.
+    /// - Note: See [Ellipse Perimeter](https://www.mathsisfun.com/geometry/ellipse-perimeter.html)
+    /// - Parameter Width: First axis. Assumed to be the long parameter but it doesn't really matter.
+    /// - Parameter Height: Second axis. Assumed to be the short parameter but it doesn't really matter.
+    /// - Returns: Circumference of the ellipse.
     public static func EllipseCircumference(Width A: Double, Height B: Double) -> Double
     {
         let Term1 = Double.pi
@@ -20,6 +24,9 @@ class Math
         return Final
     }
     
+    /// Returns the circumference of a circle.
+    /// - Parameter Radius: Radius of the circle whose circumference will be returned.
+    /// - Returns: Circumference of the circle.
     public static func CircleCircumference(Radius: Double) -> Double
     {
         return Radius * 2 * Double.pi
